c3f04f544d3acbea917b7c0798f1e85d831eacd7
Mauricio-AFLadeira <mauricioafl001@gmail.com>
2023-03-14 18:52:43 -0300

    singleton

--- src/main/java/org/example/Configuracao.java
+++ src/main/java/org/example/Configuracao.java
@@ -1,2 +1,39 @@
-package org.example;public class Configuração {
+package org.example;
+
+public class Configuracao {
+    private Configuracao() {};
+    private static Configuracao instancia = new Configuracao();
+    public static Configuracao getInstancia() {
+        return instancia;
+    }
+    private String urlBancoDados;
+    private String usuarioBancoDados;
+    private String senhaBancoDados;
+
+    public String getUrlBancoDados() {
+        return urlBancoDados;
+    }
+
+    public void setUrlBancoDados(String urlBancoDados) {
+        this.urlBancoDados = urlBancoDados;
+    }
+
+    public String getUsuarioBancoDados() {
+        return usuarioBancoDados;
+    }
+
+    public void setUsuarioBancoDados(String usuarioBancoDados) {
+        this.usuarioBancoDados = usuarioBancoDados;
+    }
+
+    public String getSenhaBancoDados() {
+        return senhaBancoDados;
+    }
+
+    public void setSenhaBancoDados(String senhaBancoDados) {
+        this.senhaBancoDados = senhaBancoDados;
+    }
+
+
+
 }
--- src/test/java/org/example/ConfiguracaoTest.java
+++ src/test/java/org/example/ConfiguracaoTest.java
@@ -1,4 +1,25 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ConfiguracaoTest {
-  
+    @Test
+    public void deveRetornarUrlDoBancoDeDados() {
+        Configuracao.getInstancia().setUrlBancoDados("localhost");
+        assertEquals("localhost", Configuracao.getInstancia().getUrlBancoDados());
+    }
+
+    @Test
+    public void deveRetornarUsuarioDoBancoDeDados() {
+        Configuracao.getInstancia().setUsuarioBancoDados("root");
+        assertEquals("root", Configuracao.getInstancia().getUsuarioBancoDados());
+    }
+
+    @Test
+    public void deveRetornarSenhaDoBancoDeDados() {
+        Configuracao.getInstancia().setUsuarioBancoDados("password");
+        assertEquals("password", Configuracao.getInstancia().getUsuarioBancoDados());
+    }
 }
